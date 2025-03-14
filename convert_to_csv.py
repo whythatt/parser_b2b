ee79f862a23c1ebe79beb26266bf7cf45008da16
whythatt <zakrogo513@gmail.com>
2025-03-14 09:11:51 +0300

    залил брянск

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -2,19 +2,11 @@ import csv
 import json
 
 # Load JSON data from the file
-<<<<<<< HEAD
-with open("company_numbers_тверь.json", "r", encoding="utf-8") as json_file:
+with open("company_numbers_брянск.json", "r", encoding="utf-8") as json_file:
     data = json.load(json_file)
 
 # Open a CSV file for writing
-with open("company_numbers_тверь.csv", "w", newline="", encoding="utf-8") as csv_file:
-=======
-with open("company_numbers_спб.json", "r", encoding="utf-8") as json_file:
-    data = json.load(json_file)
-
-# Open a CSV file for writing
-with open("company_numbers_спб.csv", "w", newline="", encoding="utf-8") as csv_file:
->>>>>>> b061450 (test)
+with open("company_numbers_брянск.csv", "w", newline="", encoding="utf-8") as csv_file:
     writer = csv.writer(csv_file)
     # Write the header
     writer.writerow(["company_name", "company_number", "category", "website"])
